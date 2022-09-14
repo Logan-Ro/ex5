67eadecca24e5787b9cbbedb16c01b25480dd317
logan <logan@LAPTOP-BO4FCM1U>
2022-09-14 11:49:59 +0200

    Ajoutez des fichiers projet.

---
+++ ex5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.WriteLine("Entrez votre âge : ");
+            age = int.Parse(Console.ReadLine());
+
+            if (age >= 18)
+            {
+                Console.WriteLine("majeur");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("mineur");
+                Console.ReadLine();
+            }
+        }
+    }
+}
